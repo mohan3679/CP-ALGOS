f3603299b83a4ffb9bd456020956549d77d61bbd
mohan3679 <72187210+mohan3679@users.noreply.github.com>
2021-10-06 22:35:21 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,14 @@
+void make_set(int v) {
+    parent[v] = v;
+    index[v] = rand();
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (index[a] < index[b])
+            swap(a, b);
+        parent[b] = a;
+    }
+}
